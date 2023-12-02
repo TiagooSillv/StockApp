65f4c8f11cae797e34b3f4ed9b36db5474ba703a
Tiago <Senai@CEPEAS21103957>
2023-12-01 21:53:12 -0300

    Acabou o turno

---
+++ src/DataAccess/StockApp.DataAccess/SqlConnection/SqlServerContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.DataAccess.SqlConnection
+{
+    internal static class SqlServerContext
+    {
+        internal static string ConexaoSemBanco => @"Data Source=.\SQLEXPRESS;User ID=sa;Password=sql2022; Trusted_Connection = False; TrustServerCertificate = True;";
+        internal static string ConexaoComBanco => @"Data Source=.\SQLEXPRESS;Initial Catalog=StockGame;User ID=sa;Password=sql2022; Trusted_Connection = False; TrustServerCertificate = True;";
+
+    }
+}
